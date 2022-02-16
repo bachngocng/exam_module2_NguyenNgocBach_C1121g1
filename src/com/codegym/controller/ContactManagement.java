4b783afd992092e7155380b8c99c37fb1debcb71
bachngocng <nnbach97@gmail.com>
2022-02-16 14:18:00 +0700

    changed

--- src/com/codegym/controller/ContactManagement.java
+++ src/com/codegym/controller/ContactManagement.java
@@ -8,6 +8,7 @@ import java.util.List;
 
 public class ContactManagement implements ReadFile, WriteFile {
     private List<Contact> contacts = new ArrayList<>();
+    private static ContactManagement contactManagement;
 
     public int size() {
         return contacts.size();
@@ -17,15 +18,15 @@ public class ContactManagement implements ReadFile, WriteFile {
         contacts.remove(contact);
     }
 
-    public int findSearchIDByName(String name) {
-        int searchID = -1;
-        for (int i = 0; i < contacts.size(); i++) {
-            if (contacts.get(i).getName().equals(name)) {
-                searchID = i;
+    public int findIndexByPhonenumber(String phonenumber) {
+        int index = -1;
+        for (int i = 0; i < this.contacts.size(); i++) {
+            if (this.contacts.get(i).getPhonenumber().equals(phonenumber)) {
+                index = i;
                 break;
             }
         }
-        return searchID;
+        return index;
     }
 
     public Contact findContactByName(String name) {
@@ -52,7 +53,6 @@ public class ContactManagement implements ReadFile, WriteFile {
         contacts.set(1, phonebook1);
     }
 
-
     public boolean deleteByName(String name) {
         Contact contact = findContactByName(name);
         if (name.equals(contact.getName())) {
@@ -63,6 +63,22 @@ public class ContactManagement implements ReadFile, WriteFile {
         }
     }
 
+    public void deleteContact(String phonenumber){
+        int index = findIndexByPhonenumber(phonenumber);
+        this.contacts.remove(index);
+    }
+
+    public void updateContact(Contact contact, String phonenumber){
+        int index = findIndexByPhonenumber(phonenumber);
+        this.contacts.set(index,contact);
+    }
+
+    public static ContactManagement getInstance() {
+        if (contactManagement == null) {
+            contactManagement = new ContactManagement();
+        }
+        return contactManagement;
+    }
 
     @Override
     public void readFile(String path) throws IOException, ClassNotFoundException {
--- src/com/codegym/view/ContactMenu.java
+++ src/com/codegym/view/ContactMenu.java
@@ -11,11 +11,10 @@ public class ContactMenu {
 
     public void run() {
         ContactManagement contactManagement = new ContactManagement();
-        contactManagement.addNew(new Contact("a","a","a","a","a"));
+        contactManagement.addNew(new Contact("a", "a", "a", "a", "a"));
         int choice = -1;
         do {
             menu();
-            System.out.println("Chọn chức năng: ");
             choice = scanner.nextInt();
             scanner.nextLine();
             switch (choice) {
@@ -58,81 +57,88 @@ public class ContactMenu {
                     break;
             }
         }
-            while (choice != 0) ;
+        while (choice != 0);
+    }
+
+
+    private void updateContact(ContactManagement contactManagement) {
+        System.out.println("---Cập nhật thông tin số điên thaoị");
+        System.out.println("Nhập số điện thoại bạn muốn thay đổi");
+        String phonenumber = scanner.nextLine();
+        int index = contactManagement.findIndexByPhonenumber(phonenumber);
+        if (index == -1) {
+            System.out.println("Không tìm được danh bạ với số điện thoại trên");
+        } else {
+            Contact contact = inputNewContactInfo();
+            contactManagement.updateContact(contact, phonenumber);
+            System.out.println("Đã cập nhật");
         }
+    }
 
+    private void deleteContact(ContactManagement contactManagement) {
+        System.out.println(("---Xóa---"));
+        System.out.println("Nhập danh bạ mà bạn muốn xóa:");
+        String name = scanner.nextLine();
+        System.out.println("Bạn chắc chắn muốn xóa?(Y/N?)");
+        String deleteChoice = scanner.nextLine();
+        if (deleteChoice.equals("Y")) {
+            Contact contact = contactManagement.findContactByName(name);
+            contactManagement.removeContact(contact);
+        } else if (deleteChoice.equals("N")) {
 
-        private void updateContact(ContactManagement phonebookManagement){
-            System.out.println("---Cập nhật thông tin số điên thaoị");
-            System.out.println("Nhập tên contact bạn muốn thay đổi");
-            String searchName = scanner.nextLine();
-            phonebookManagement.findContactByName(searchName);
-            System.out.println("Cập nhật thất bại do ko tìm thấy sdt");
+        } else {
+
         }
-
-        private void deleteContact(ContactManagement contactManagement){
-            System.out.println(("---Xóa---"));
-            System.out.println("Nhập danh bạ mà bạn muốn xóa:");
-            String name = scanner.nextLine();
-            System.out.println("Bạn chắc chắn muốn xóa?(Y/N?)");
-            String deleteChoice = scanner.nextLine();
-                if(deleteChoice.equals("Y")){
-                    Contact contact = contactManagement.findContactByName(name);
-                    contactManagement.removeContact(contact);
-                } else if (deleteChoice.equals("N")){
-
-                } else {
-
-                }
-        }
+    }
 
         private void findContact(ContactManagement contactManagement){
             System.out.println("---Tìm kiếm---");
             System.out.println("Nhập tên liên lạc bạn muốn tìm: ");
             String name = scanner.nextLine();
-            Contact contact = contactManagement.findContactByName(name);
-            if (name.equals(contact.getName())) {
-                System.out.println("Thông tin danh bạ cần tìm" + "\"" + contact);
-            }
-        }
-
-        private Contact inputNewContactInfo(){
-            System.out.println("Nhập họ và tên");
-            scanner.nextLine();
-            String name = scanner.nextLine();
-            System.out.println("Nhập số điện thoại");
-            String phoneNumber = scanner.nextLine();
-            System.out.println("Nhập địa chỉ người dùng");
-            String address = scanner.nextLine();
-            System.out.println("Nhập email người dùng");
-            String email = scanner.nextLine();
-            System.out.println("Nhập facebook người dùng(");
-            String facebook = scanner.nextLine();
-            Contact contact = new Contact(name, phoneNumber, address, email, facebook);
-            return contact;
-        }
-
-        private void showContact(ContactManagement phonebookManagement){
-            int size = phonebookManagement.size();
-            if (size == 0) {
-                System.out.println("Danh bạ trống");
+            contactManagement.findContactByName(name);
+            if(contactManagement.findContactByName(name) == null){
+                System.out.println("Không tìm thấy !!!");
             } else {
-                System.out.println("---Danh bạ---");
-                phonebookManagement.displayAll();
+                System.out.println("Tìm thấy!!");
             }
-        }
+    }
 
-        private void menu () {
-            System.out.println("---CHƯƠNG TRÌNH QUẢN LÝ DANH BẠ---");
-            System.out.println("Chọn chức năng theo số(để tiếp tục)");
-            System.out.println("1. Xem danh sách");
-            System.out.println("2. Thêm mới");
-            System.out.println("3. Cập Nhật");
-            System.out.println("4. Xóa");
-            System.out.println("5. Tìm kiếm");
-            System.out.println("6. Đọc từ file");
-            System.out.println("7. Ghi vào file");
-            System.out.println("0. Thoát");
-            System.out.println("Chọn chức năng");
+    private Contact inputNewContactInfo() {
+        System.out.println("Nhập họ và tên");
+        String name = scanner.nextLine();
+        System.out.println("Nhập số điện thoại");
+        String phoneNumber = scanner.nextLine();
+        System.out.println("Nhập địa chỉ người dùng");
+        String address = scanner.nextLine();
+        System.out.println("Nhập email người dùng");
+        String email = scanner.nextLine();
+        System.out.println("Nhập facebook người dùng(");
+        String facebook = scanner.nextLine();
+        Contact contact = new Contact(name, phoneNumber, address, email, facebook);
+        return contact;
+    }
+
+    private void showContact(ContactManagement phonebookManagement) {
+        int size = phonebookManagement.size();
+        if (size == 0) {
+            System.out.println("Danh bạ trống");
+        } else {
+            System.out.println("---Danh bạ---");
+            phonebookManagement.displayAll();
         }
     }
+
+    private void menu() {
+        System.out.println("---CHƯƠNG TRÌNH QUẢN LÝ DANH BẠ---");
+        System.out.println("Chọn chức năng theo số(để tiếp tục)");
+        System.out.println("1. Xem danh sách");
+        System.out.println("2. Thêm mới");
+        System.out.println("3. Cập Nhật");
+        System.out.println("4. Xóa");
+        System.out.println("5. Tìm kiếm");
+        System.out.println("6. Đọc từ file");
+        System.out.println("7. Ghi vào file");
+        System.out.println("0. Thoát");
+        System.out.println("Chọn chức năng: ");
+    }
+}
